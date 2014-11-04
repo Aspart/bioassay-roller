2ca664d4a823b56c8dec3df4a86813cf5ebe6239
Roman Degtyarev <rdegtiarev@gmail.com>
2014-11-04 15:32:01 +0300

    Raw data processing via DRC works fine now

--- data_prep.R
+++ data_prep.R
@@ -1,2 +1,55 @@
-test = raw_n[c(F,T),]
-ref = raw_n[c(T,F),ncol(raw_n):1]
+library(plyr)
+library(gtools)
+library(ggplot2)
+library(reshape2)
+library(plyr)
+library(scales)
+library(drc)
+
+load.raw = function(file) {
+  data = read.table(
+    file=file,
+    sep=';',
+    header=F,
+    row.names = NULL)
+  data = data[data$V1 != '', ]
+  data = data[data$V1 != 'A' & data$V1 !='H' & data$V1 != '<>', ]
+  data = subset(data, select=-c(V2, V13))
+  list.df = split(subset(data[!is.na(data$V3),], select=-c(V1)), (0:(nrow(data[!is.na(data$V3),])-1) %/% 6))  # modulo division
+  names(list.df) = as.factor(as.vector(data[seq(1, nrow(data), 7),1]))
+  list.df = lapply(list.df, function(df) {rownames(df) <- NULL; df})
+  list.df
+}
+
+df.to.comparable = function(df, doses) {
+  test = df[c(F,T),]
+  colnames(test) = doses
+  rownames(test) = NULL
+  ref = df[c(T,F),ncol(df):1]
+  colnames(ref) = doses
+  rownames(ref) = NULL
+  result = list(ref,test)
+  names(result) = c('ref', 'test')
+  result
+}
+
+melt.comparable.df = function(df) {
+  test = melt(df$test, variable.name='dose', value.name='responce')
+  test$sample = 'test'
+  ref = melt(df$ref, variable.name='dose', value.name='responce')
+  ref$sample = 'ref'
+  result = rbind(test,ref)
+  result$dose = as.numeric(as.character(result$dose))
+  result
+}
+
+drm.model = function(df) {
+  drm(responce~dose, sample, data = df, fct = LL.4(names=c("Slope","Lower Limit","Upper Limit", "ED50")))
+}
+
+doses = round(150 / (2^(0:9)), 2)
+df.list = load.raw('/Users/roman/Projects/Biocad/bioassay-raw/bev_281014/raw.csv')
+df.list = lapply(df.list, df.to.comparable, doses)
+df.list = lapply(df.list, melt.comparable.df)
+result = lapply(df.list, drm.model)
+lapply(result, SI, c(50,50), ci = "delta")
--- main.R
+++ main.R
@@ -12,13 +12,12 @@ myrnorm = function(mean, sd, n) {
 
 ######################################
 # lets model some dose-response data #
-sample.4LP = function(A,B,C,D,e, rp = 1) {
+
+sample.4LP = function(B, A, D, C, e, rp = 1, rep=3) {
   # concentration params
   z = 150 / (2^(0:9)) # doses
-  # samples from each dose
-  rep = 3
   # samples
-  y = D + (D-A)/(1+(z/C)^B)
+  y = D + (A-D)/(1+(rp*z/C)^B)
   data = sapply(y, myrnorm, sd=e, n=rep)
   colnames(data) = z
   melt.df = melt(data)[,2:3]
@@ -28,7 +27,8 @@ sample.4LP = function(A,B,C,D,e, rp = 1) {
 
 dose.mean = function(melt.df) {
   # calculate mean for each concentration point
-  splitted <- ddply(melt.df, .(dose, variable), summarise, mean = round(mean(value), 2))
+  cn = colnames(melt.df)
+  splitted <- ddply(melt.df, c(cn[1], cn[2]), summarise, mean = round(mean(cn[2]), 2))
   mean = dcast(splitted, dose ~ variable, value.var="mean")
   mean
 }
@@ -42,12 +42,13 @@ dose.sd = function(melt.df) {
 
 # model params for 4LP from 'Two Approaches to Potency Bioassay Analysis' Harry Yang, Ph.D
 B = 1.36 # slope parameter
-A = 73454 # lower asymptotes
-D = 415000 # upper asymptotes
+D = 6.42 # lower asymptotes
+A = 9.19 # upper asymptotes
 C = 7.98 # EC50
-e = 6000 # TODO: reference SD = 2 * test SD
+e = 0.05 # TODO: reference SD = 2 * test SD
 
-data = sample.4LP(A,B,C,D,e)
+data = sample.4LP(B, A, D, C, e, rp, rep)
 
 model = drm(responce~dose, data = data, fct = LL.4(names=c("Slope","Lower Limit","Upper Limit", "ED50")))
 plot(model)
+summary(model)
