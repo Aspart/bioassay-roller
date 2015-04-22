05b6551e21dee8184df55d73a4c654625ceccedc
Roman Degtyarev <rdegtiarev@gmail.com>
2015-04-22 18:07:47 +0300

    Generalized ESD outlier test added

--- R/OutlierProcessor.R
+++ R/OutlierProcessor.R
@@ -1,9 +1,30 @@
 # Remove outliers from df provided --------
 outlier.removal = function(df) {
   res = lapply(split(df, df$dose), function(x) {
-    outlier_tf = outlier(x$response,logical=TRUE)
-    find_outlier = which(outlier_tf==TRUE,arr.ind=TRUE)
-    x = x[-find_outlier,]
+    outliers = ESD.test(x$response, max=3)
+    x = x[!outliers,]
   })
-  do.call(rbind, res)
+  new.df = do.call(rbind, res)
+  rownames(new.df) = NULL
+  new.df
+}
+
+
+ESD.test = function(x, max, alpha=0.05) {
+  n <- length(x)
+  result = rep(F, n)
+  subx <- data.frame(row=as.integer(rownames(as.data.frame(x))), x)
+  outliers = data.frame()
+  for(i in 1:max) {
+    r <- abs(subx$x - mean(subx$x))/sd(subx$x)
+    p <- 1 - alpha/(2*(n-i+1))
+    t <- qt(p, (n-i-1))
+    l <- t*(n-i) / sqrt((n-i-1+t**2)*(n-i+1))
+    outliers <- as.data.frame(rbind(outliers, list(row=subx[which.max(r),'row'], r=max(r), l=l)))
+    if(max(r)>l) {
+      result[subx[which.max(r), 'row']] <- T
+    }
+    subx <- as.data.frame(subx[-which.max(r),])
+  }
+  result
 }
